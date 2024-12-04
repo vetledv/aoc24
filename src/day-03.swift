2afe66f1e88c5c2c35d32b80c752c817e4962de4
Vetle de Vries Brandth <vetle@dvries.com>
2024-12-04 20:28:32 +0100

    day03 part 2

--- src/day-03.swift
+++ src/day-03.swift
@@ -6,6 +6,7 @@ struct Day03: Day {
 
     let validMulRegex = /(mul\([0-9]+,[0-9]+\))/
     let numRegex = /[0-9]+/
+    let mulInstructionsRegex = /(mul\([0-9]+,[0-9]+\))|don't\(\)|(do\(\))/
 
     func part1() -> Any {
         let validMuls = data.matches(of: validMulRegex).map { String($0.0) }
@@ -13,7 +14,24 @@ struct Day03: Day {
     }
 
     func part2() -> Any {
-        return ""
+        let validMuls = data.matches(of: mulInstructionsRegex).map { String($0.0) }
+        var result = 0
+        var enabled = true
+
+        for part in validMuls {
+            if part == "don't()" {
+                enabled = false
+                continue
+            }
+            if part == "do()" {
+                enabled = true
+                continue
+            }
+            if enabled {
+                result += findNumbers(data: part).reduce(1, *)
+            }
+        }
+        return result
     }
 
     private func findNumbers(data: String) -> [Int] {
